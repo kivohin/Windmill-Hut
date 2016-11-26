a1ea1b433f34ef4e4e01afd0e0007e2f16f4b63e
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-25 23:38:05 -0500

    Add Webpack Nodem modules. Modify Webpack config file. Create main index files.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + 'app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: [
     './app/index.js'
+  ],
+
+  module: {
+    loaders: [
+
+    ]
+  },
+
+  output: {
+    filename: 'index_bundle.js',
+    path: __dirname + '/dist'
+  },
+
+  plugins: [
+    HtmlWebpackPluginConfig
   ]
 }
